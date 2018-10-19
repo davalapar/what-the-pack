5faae203c600e3b9c678cb837e5728e2ed6fd44a
davalapar <davalapar@gmail.com>
2018-10-19 17:03:01 +0800

    add rollup config + umd build

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+const { terser } = require('rollup-plugin-terser');
+const commonjs = require('rollup-plugin-commonjs');
+const node = require('rollup-plugin-node-resolve');
+
+export default {
+  input: 'browser.js',
+  output: {
+    file: 'dist/MessagePack.min.js',
+    format: 'umd',
+    name: 'MessagePack',
+    sourcemap: true
+  },
+  plugins: [node({ preferBuiltins: false }), commonjs(), terser()]
+}
